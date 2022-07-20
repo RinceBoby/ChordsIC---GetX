c48ac0ce5ec739235ea5bb3518ac12e60f19962b
RinceBoby <101865864+RinceBoby@users.noreply.github.com>
2022-07-20 19:57:39 +0530

    second

--- lib/views/playlist/playlistsongs.dart
+++ lib/views/playlist/playlistsongs.dart
@@ -67,7 +67,8 @@ class PlayListSongs extends StatelessWidget {
 
       //<<<<<Body>>>>>//
       body: Center(
-        child: GetBuilder<PlaylistContoller>(
+        child: 
+        GetBuilder<PlaylistContoller>(
           init: PlaylistContoller(),
           builder: (controller) {
             return FutureBuilder<List<SongEntity>>(
@@ -87,125 +88,129 @@ class PlayListSongs extends StatelessWidget {
                     ),
                   );
                 }
-                List<SongEntity> playlistsongs = item.data!;
 
-                List<Audio> playListList = [];
+                return GetBuilder<PlaylistContoller>(builder: (controller) {
+                  List<SongEntity> playlistsongs = item.data!;
 
-                for (var songs in playlistsongs) {
-                  playListList.add(
-                    Audio.file(
-                      songs.lastData,
-                      metas: Metas(
-                        title: songs.title,
-                        artist: songs.artist,
-                        id: songs.id.toString(),
-                      ),
-                    ),
-                  );
-                }
-                return ListView.builder(
-                  itemCount: item.data!.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                      child: Container(
-                        height: 90,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(10),
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 0.3,
-                              blurRadius: 1,
-                              offset: const Offset(3, 3),
-                            ),
-                          ],
-                          gradient: LinearGradient(
-                            colors: [
-                              Colors.purpleAccent.withOpacity(0.3),
-                              Colors.purpleAccent.withOpacity(0.015),
-                            ],
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                          ),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                          child: ListTile(
-                            onLongPress: () async {
-                              await playlistContoller.delPlSongs(
-                                playlistsongs: playlistsongs,
-                                index: index,
-                                playlistkey: playlistkey,
-                              );
-                              // audioRoom.deleteFrom(
-                              //     RoomType.PLAYLIST, playlistsongs[index].id,
-                              //     playlistKey: playlistkey);
-                              Get
-                                ..closeAllSnackbars()
-                                ..showSnackbar(
-                                  const GetSnackBar(
-                                    backgroundColor:
-                                        Color.fromARGB(255, 219, 147, 232),
-                                    //title: "🎶",
-                                    icon: Icon(
-                                      Icons.playlist_remove,
-                                      color: Colors.grey,
-                                    ),
-                                    message: "Song Removed From Playlist",
-                                    duration: Duration(seconds: 2),
-                                  ),
-                                );
-                            },
-                            onTap: () async {
-                              await playlistContoller.playPlSongs(
-                                index: index,
-                                playlistSongs: playListList,
-                              );
-                            },
-                            leading: SizedBox(
-                              width: 60,
-                              height: 60,
-                              child: QueryArtworkWidget(
-                                id: item.data![index].id,
-                                type: ArtworkType.AUDIO,
-                                nullArtworkWidget: ClipRRect(
-                                  borderRadius: const BorderRadius.all(
-                                    Radius.circular(300),
-                                  ),
-                                  child: Image.asset(
-                                      'assets/images/Apple-Music-Artist-Lover.png'),
-                                ),
-                              ),
-                            ),
-                            title: Text(
-                              item.data![index].title,
-                              overflow: TextOverflow.ellipsis,
-                              style: GoogleFonts.nunito(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black,
-                              ),
-                            ),
-                            subtitle: Text(
-                              item.data![index].artist.toString(),
-                              overflow: TextOverflow.ellipsis,
-                              //"${item.data![index].artist}",//
-                              //====This is another way without toString()====//
-                              style: GoogleFonts.nunito(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
+                  List<Audio> playListList = [];
+
+                  for (var songs in playlistsongs) {
+                    playListList.add(
+                      Audio.file(
+                        songs.lastData,
+                        metas: Metas(
+                          title: songs.title,
+                          artist: songs.artist,
+                          id: songs.id.toString(),
                         ),
                       ),
                     );
-                  },
-                );
+                  }
+
+                  return ListView.builder(
+                    itemCount: item.data!.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                        child: Container(
+                          height: 90,
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                spreadRadius: 0.3,
+                                blurRadius: 1,
+                                offset: const Offset(3, 3),
+                              ),
+                            ],
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.purpleAccent.withOpacity(0.3),
+                                Colors.purpleAccent.withOpacity(0.015),
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                            child: ListTile(
+                              onLongPress: () async {
+                                await playlistContoller.delPlSongs(
+                                  playlistsongs: playlistsongs,
+                                  index: index,
+                                  playlistkey: playlistkey,
+                                );
+                                // audioRoom.deleteFrom(
+                                //     RoomType.PLAYLIST, playlistsongs[index].id,
+                                //     playlistKey: playlistkey);
+                                Get
+                                  ..closeAllSnackbars()
+                                  ..showSnackbar(
+                                    const GetSnackBar(
+                                      backgroundColor:
+                                          Color.fromARGB(255, 219, 147, 232),
+                                      //title: "🎶",
+                                      icon: Icon(
+                                        Icons.playlist_remove,
+                                        color: Colors.grey,
+                                      ),
+                                      message: "Song Removed From Playlist",
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  );
+                              },
+                              onTap: () async {
+                                await playlistContoller.playPlSongs(
+                                  index: index,
+                                  playlistSongs: playListList,
+                                );
+                              },
+                              leading: SizedBox(
+                                width: 60,
+                                height: 60,
+                                child: QueryArtworkWidget(
+                                  id: item.data![index].id,
+                                  type: ArtworkType.AUDIO,
+                                  nullArtworkWidget: ClipRRect(
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(300),
+                                    ),
+                                    child: Image.asset(
+                                        'assets/images/Apple-Music-Artist-Lover.png'),
+                                  ),
+                                ),
+                              ),
+                              title: Text(
+                                item.data![index].title,
+                                overflow: TextOverflow.ellipsis,
+                                style: GoogleFonts.nunito(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              subtitle: Text(
+                                item.data![index].artist.toString(),
+                                overflow: TextOverflow.ellipsis,
+                                //"${item.data![index].artist}",//
+                                //====This is another way without toString()====//
+                                style: GoogleFonts.nunito(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                });
               },
             );
           },
